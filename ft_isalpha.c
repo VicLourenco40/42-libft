519cf8ecc62baefd1d76091bd0ee36dc8fff6da6
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2025-04-04 14:19:57 +0100

    Add function ft_isalpha

---
+++ ft_isalpha.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-albu <vde-albu@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/04 11:00:07 by vde-albu          #+#    #+#             */
+/*   Updated: 2025/04/04 14:13:37 by vde-albu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static int	isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
+
+static int	islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
+
+int	ft_isalpha(int c)
+{
+	return (isupper(c) || islower(c));
+}
