f0cfa28f27ed5c012b2613a6b36df60144ca2345
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2025-04-07 13:23:54 +0100

    Add function ft_strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-albu <vde-albu@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/07 12:33:37 by vde-albu          #+#    #+#             */
+/*   Updated: 2025/04/07 13:23:21 by vde-albu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	little_len;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (!*little)
+		return ((char *) big);
+	little_len = ft_strlen(little);
+	while (big[i] && i + little_len <= len)
+	{
+		if (big[i] == *little)
+		{
+			j = 0;
+			while (big[i + j] && little[j] && big[i + j] == little[j])
+				j++;
+			if (j == little_len)
+				return ((char *) &big[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vde-albu <vde-albu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 13:33:53 by vde-albu          #+#    #+#             */
-/*   Updated: 2025/04/07 12:17:07 by vde-albu         ###   ########.fr       */
+/*   Updated: 2025/04/07 12:34:15 by vde-albu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,6 @@ char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 
 #endif
