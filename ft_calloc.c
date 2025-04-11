f6e5029c6e589546d45932545ae5e9356d64dd76
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2025-04-11 17:25:41 +0100

    Fix integer overflow check on ft_calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,25 +6,23 @@
 /*   By: vde-albu <vde-albu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 09:29:15 by vde-albu          #+#    #+#             */
-/*   Updated: 2025/04/08 14:39:20 by vde-albu         ###   ########.fr       */
+/*   Updated: 2025/04/11 17:24:26 by vde-albu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <limits.h>
+#include <stdint.h>
 #include "libft.h"
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
-	size_t			total_size;
-	unsigned char	*ptr;
+	void	*ptr;
 
-	total_size = nmemb * size;
-	if (total_size > INT_MAX)
+	if (size && nmemb > (SIZE_MAX / size))
 		return (NULL);
-	ptr = malloc(total_size);
+	ptr = malloc(nmemb * size);
 	if (!ptr)
 		return (NULL);
-	ft_bzero(ptr, total_size);
+	ft_bzero(ptr, nmemb * size);
 	return (ptr);
 }
