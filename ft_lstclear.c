783ee01d607b7010805f62af0b9e4c256ee9b297
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2025-04-11 14:56:16 +0100

    Add function ft_lstclear

---
+++ ft_lstclear.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-albu <vde-albu@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/11 14:22:03 by vde-albu          #+#    #+#             */
+/*   Updated: 2025/04/11 14:54:36 by vde-albu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	if (!*lst)
+		return ;
+	if ((*lst)->next)
+		ft_lstclear(&(*lst)->next, del);
+	if ((*lst)->content)
+		del((*lst)->content);
+	free(*lst);
+	*lst = NULL;
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: vde-albu <vde-albu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 13:33:53 by vde-albu          #+#    #+#             */
-/*   Updated: 2025/04/11 12:10:54 by vde-albu         ###   ########.fr       */
+/*   Updated: 2025/04/11 14:21:48 by vde-albu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,5 +60,6 @@ int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
 
 #endif
