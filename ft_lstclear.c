728109b1aea12408ff87f6423fc01827befaf032
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2025-04-11 17:37:28 +0100

    Remove redundant NULL checks on ft_lstclear

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: vde-albu <vde-albu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 14:22:03 by vde-albu          #+#    #+#             */
-/*   Updated: 2025/04/11 14:54:36 by vde-albu         ###   ########.fr       */
+/*   Updated: 2025/04/11 17:19:52 by vde-albu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,8 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	if (!*lst)
 		return ;
-	if ((*lst)->next)
-		ft_lstclear(&(*lst)->next, del);
-	if ((*lst)->content)
-		del((*lst)->content);
+	ft_lstclear(&(*lst)->next, del);
+	del((*lst)->content);
 	free(*lst);
 	*lst = NULL;
 }
