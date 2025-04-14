8902b13380630b7b9b5fbbd54757dea82eedcad6
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2025-04-14 14:21:31 +0100

    Compare using unsigned chars on ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: vde-albu <vde-albu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 11:19:00 by vde-albu          #+#    #+#             */
-/*   Updated: 2025/04/07 11:43:59 by vde-albu         ###   ########.fr       */
+/*   Updated: 2025/04/14 14:19:57 by vde-albu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,17 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	unsigned char	u1;
+	unsigned char	u2;
+	size_t			i;
 
 	i = 0;
 	while ((s1[i] || s2[i]) && i < n)
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		u1 = s1[i];
+		u2 = s2[i];
+		if (u1 != u2)
+			return (u1 - u2);
 		i++;
 	}
 	return (0);
