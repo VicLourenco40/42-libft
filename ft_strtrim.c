3e660095ed30e9bbe91e6d6a0671a897b79df4e6
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2025-04-14 14:58:07 +0100

    Redo function ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: vde-albu <vde-albu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 10:19:21 by vde-albu          #+#    #+#             */
-/*   Updated: 2025/04/09 17:41:00 by vde-albu         ###   ########.fr       */
+/*   Updated: 2025/04/14 14:54:59 by vde-albu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,26 @@
 
 char	*ft_strtrim(const char *s1, const char *set)
 {
-	unsigned int	i;
-	unsigned int	len;
-	char			*new;
+	size_t	s1_len;
+	size_t	start;
+	size_t	end;
+	size_t	len;
+	char	*new;
 
-	i = 0;
-	while (s1[i] && ft_strchr(set, s1[i]))
-		i++;
-	len = 0;
-	while (s1[i + len] && !ft_strchr(set, s1[i + len]))
-		len++;
+	s1_len = ft_strlen(s1);
+	start = 0;
+	while (start < s1_len && ft_strchr(set, s1[start]))
+		start++;
+	end = 0;
+	while (end < s1_len && ft_strchr(set, s1[s1_len - end - 1]))
+		end++;
+	if (start + end < s1_len)
+		len = s1_len - start - end;
+	else
+		len = 1;
 	new = malloc(sizeof(char) * (len + 1));
 	if (!new)
 		return (NULL);
-	ft_strlcpy(new, &s1[i], len + 1);
+	ft_strlcpy(new, &s1[start], len + 1);
 	return (new);
 }
