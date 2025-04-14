1b8d1a0c08002228da9423673b25accfb5301771
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2025-04-14 13:30:50 +0100

    Check if del function exists instead of content

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: vde-albu <vde-albu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 14:22:03 by vde-albu          #+#    #+#             */
-/*   Updated: 2025/04/11 18:55:07 by vde-albu         ###   ########.fr       */
+/*   Updated: 2025/04/14 13:30:02 by vde-albu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	if (!lst || !*lst)
 		return ;
 	ft_lstclear(&(*lst)->next, del);
-	if ((*lst)->content)
+	if (del)
 		del((*lst)->content);
 	free(*lst);
 	*lst = NULL;
--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: vde-albu <vde-albu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 12:11:04 by vde-albu          #+#    #+#             */
-/*   Updated: 2025/04/11 14:19:39 by vde-albu         ###   ########.fr       */
+/*   Updated: 2025/04/14 13:29:08 by vde-albu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
 	if (!lst)
 		return ;
-	if (lst->content)
+	if (del)
 		del(lst->content);
 	free(lst);
 }
