b15e6d0b8f06af3b00bb7b5127548e5c0068b342
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2025-05-14 19:10:12 +0100

    Revert BUFFER_SIZE

--- include/ft_get_next_line.h
+++ include/ft_get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: vde-albu <vde-albu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 11:40:07 by vde-albu          #+#    #+#             */
-/*   Updated: 2025/05/13 16:50:44 by vde-albu         ###   ########.fr       */
+/*   Updated: 2025/05/14 19:00:06 by vde-albu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_GET_NEXT_LINE_H
 # define FT_GET_NEXT_LINE_H
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1
+#  define BUFFER_SIZE 42
 # endif
 
 char	*ft_get_next_line(int fd);
